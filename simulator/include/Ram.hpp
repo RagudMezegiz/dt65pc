419d3cde865bc2245669c2af5295b764f2cbeec5
David Terhune <dave.terhune@gmail.com>
2023-03-26 15:25:11 -0600

    Make RAM bank-aware

--- simulator/include/Ram.hpp
+++ simulator/include/Ram.hpp
@@ -1,25 +1,50 @@
-//
-// Created by Francesco Rigoni on 05/03/2018.
-//
-
-#ifndef LIB65816_SAMPLE_PROGRAMRAM_H
-#define LIB65816_SAMPLE_PROGRAMRAM_H
+// Copyright (C) 2018 Francesco Rigoni
+// Copyright (C) 2023 David Terhune
+// 
+// This file is part of dt65pc.
+// https://github.com/RagudMezegiz/dt65pc
+// 
+// dt65pc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// dt65pc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with dt65pc.  If not, see <http://www.gnu.org/licenses/>.
+#ifndef RAM_HPP
+#define RAM_HPP
 
 #include "SystemBusDevice.hpp"
 
+/// @brief RAM chip of arbitrary size.
+/// @details
+/// Base address is hard-coded to 0, so there can be only one RAM device
+/// in the entire system. That means that a shadow RAM exists beneath every
+/// other device with an overlapping address, so RAM must be defined last
+/// in the machine description to give priority to other memory-mapped devices
+/// on read operations.
 class Ram : public SystemBusDevice {
-private:
-    uint8_t *mRam;
-
 public:
-
-    Ram(uint8_t);
+    /// @brief Constructor
+    /// @param banks Number of 64K banks
+    Ram(uint8_t banks);
     ~Ram();
 
     void storeByte(const Address &, uint8_t);
     uint8_t readByte(const Address &);
     bool decodeAddress(const Address &, Address &);
+
+private:
+    // Number of banks.
+    uint8_t mBanks;
+    
+    // Raw bytes.
+    uint8_t *mRam;
 };
 
-
-#endif //LIB65816_SAMPLE_PROGRAMRAM_H
+#endif //RAM_HPP
--- simulator/src/Ram.cpp
+++ simulator/src/Ram.cpp
@@ -1,10 +1,24 @@
-//
-// Created by Francesco Rigoni on 05/03/2018.
-//
-
+// Copyright (C) 2018 Francesco Rigoni
+// Copyright (C) 2023 David Terhune
+// 
+// This file is part of dt65pc.
+// https://github.com/RagudMezegiz/dt65pc
+// 
+// dt65pc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// dt65pc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with dt65pc.  If not, see <http://www.gnu.org/licenses/>.
 #include "Ram.hpp"
 
-Ram::Ram(uint8_t banks) {
+Ram::Ram(uint8_t banks) : mBanks(banks) {
     mRam = new uint8_t[banks * BANK_SIZE_BYTES];
 }
 
@@ -13,14 +27,14 @@ Ram::~Ram() {
 }
 
 void Ram::storeByte(const Address &address, uint8_t value) {
-    mRam[address.getOffset()] = value;
+    mRam[address.getBank() * BANK_SIZE_BYTES + address.getOffset()] = value;
 }
 
 uint8_t Ram::readByte(const Address &address) {
-    return mRam[address.getOffset()];
+    return mRam[address.getBank() * BANK_SIZE_BYTES + address.getOffset()];
 }
 
 bool Ram::decodeAddress(const Address &in, Address &out) {
     out = in;
-    return true;
+    return in.getBank() < mBanks;
 }

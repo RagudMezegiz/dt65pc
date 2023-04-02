1135afa658848e22d982aa7f4e09dd58ae0dd30b
David Terhune <dave.terhune@gmail.com>
2023-04-01 21:09:15 -0600

    Create skeletal terminal emulator

---
+++ simulator/include/Terminal.hpp
@@ -0,0 +1,26 @@
+// Copyright (C) 2023 David Terhune
+// 
+// This file is part of dt65pc.
+// 
+// dt65pc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// dt65pc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with dt65pc.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef TERMINAL_HPP_INCLUDED
+#define TERMINAL_HPP_INCLUDED
+
+/// @brief Serial terminal acting as the remote device connected to the UART.
+class Terminal {
+
+};
+
+#endif // TERMINAL_HPP_INCLUDED
